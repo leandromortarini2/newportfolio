89f0d0501e4fb47ebff4d587887dd6d19916f2a4
leandro mortarini <leandromortarini2@gmail.com>
2024-04-28 12:21:32 -0300

    asd

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -12,29 +12,43 @@ import Perfil from "@/assets/perfil.png";
 
 export const Home = () => {
   return (
-    <div className="containerHome min-h-screen flex flex-col items-center">
+    <div className="containerHome w-full min-h-screen flex flex-col items-center ">
       <NavBar />
 
-      <div className="w-full sm:w-3/5 flex flex-col sm:flex-row">
+      {/* IMAGEN CELULAR */}
+      <div className="w-1/2 sm:hidden flex justify-center sm:items-end items-center">
+        <div className=" ">
+          <Image src={Perfil} alt="" className="ImgPerfil w-40 " />
+        </div>
+      </div>
+      {/* container general */}
+      {/* container general */}
+      {/* container general */}
+
+      <div className="w-full flex justify-around ">
         {/* CONTAINER 1 */}
         {/* CONTAINER 1 */}
-        <div className="sm:w-1/2  flex flex-col justify-center items-center">
+        <div className="sm:w-1/2 md:w-1/2 flex flex-col justify-center items-center sm:items-end ">
           {/*  */}
-          <div className="w-full sm:h-[200px] flex flex-col justify-evenly items-center sm:items-start ">
-            <h2 className="p-2 sm:text-lg md:text-2xl font-normal">HOLA SOY</h2>
-            <h1 className="p-2 text-2xl md:text-3xl font-bold">
+          <div className="sm:w-3/4  flex flex-col justify-evenly items-center sm:items-start  ">
+            <h2 className="p-2 sm:text-lg md:text-2xl font-normal  text-justify">
+              HOLA SOY
+            </h2>
+            <h1 className="p-2 sm:text-2xl md:text-5xl font-bold  text-justify">
               Leandro Mortarini
             </h1>
-            <h2 className="p-2 sm:text-xl md:text-2xl">Full Stack Developer</h2>
-            <h3 className="p-2 sm:text-xl md:text-2xl text-violeta">
+            <h2 className="p-2 sm:text-lg md:text-2xl ">
+              Full Stack Developer
+            </h2>
+            <h3 className="p-2 sm:text-lg md:text-xl text-violeta ">
               leandromortarini2@gmail.com
             </h3>
           </div>
 
-          <div className="sm:w-full h-20 flex justify-start ">
+          <div className="sm:w-3/4 h-20 flex justify-start ">
             <button className="">
               {" "}
-              <Image src={GitHub} alt="" className="w-7 sm:w-8 md:w-10 ml-2" />
+              <Image src={GitHub} alt="" className="w-7 sm:w-8 md:w-10 " />
             </button>{" "}
             <button>
               {" "}
@@ -71,13 +85,14 @@ export const Home = () => {
           </div>
 
           {/* CONTAINER 2 */}
+          {/* CONTAINER 2 */}
         </div>
-        <div className="md:w-1/2  flex justify-center items-center ">
-          <div className="sm:w-1/2 ">
+        <div className="hidden sm:w-1/2  sm:flex justify-center sm:items-end items-center ">
+          <div className=" ">
             <Image
               src={Perfil}
               alt=""
-              className="ImgPerfil w-40 mb-10 sm:w-3/4 md:w-full sm:mt-20 opacity-80 z-0"
+              className="ImgPerfil w-40 sm:w-1/2 sm:ml-9 md:m-0"
             />
           </div>
         </div>
