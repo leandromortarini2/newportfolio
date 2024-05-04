2e38fda422fd8f58666b75d2e71f51f1173a6a29
leandro mortarini <leandromortarini2@gmail.com>
2024-05-04 00:18:50 -0300

    responsive

--- src/app/projects/Projects.tsx
+++ src/app/projects/Projects.tsx
@@ -22,101 +22,35 @@ export const Projects = () => {
           />
         </div>
         {/* CARDS */}
-        <div className="w-full flex flex-wrap justify-evenly">
-          <div className="w-1/2 h-[200px] bg-slate-900 rounded-xl overflow-hidden  m-5 flex ">
+        <div className="w-full flex  flex-wrap justify-evenly ">
+          <div className="w-3/4 flex flex-col md:flex-row md:w-3/4 lg:w-3/4 xl:w-1/2 md:h-[200px] bg-slate-900 rounded-xl overflow-hidden  m-5 shadow-lg shadow-teal-950">
             <Image
               src={projectImg}
               alt=""
-              className="w-1/3 h-full opacity-80 "
+              className="w-full md:w-1/3 h-full opacity-80 "
             />
             <div className="flex flex-col justify-center items-center">
               {" "}
-              <h2 className="m-2 text-lg font-semibold">E-commerce</h2>
-              <p className="pl-5 pr-5 ">
+              <h2 className="  mt-2  lg:text-lg xl:font-semibold">
+                E-commerce
+              </h2>
+              <p className="md:text-sm lg:text-md p-2 xl:pl-5 xl:pr-5 ">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut
                 commodi blanditiis dolorum facere! Magnam ducimus eveniet
                 quisquam quibusdam provident velit? Quasi vitae obcaecati
                 dignissimos consequatur illo culpa numquam ducimus accusantium.
               </p>
-              <div className="w-full flex justify-end mb-2">
-                <div className="w-1/4 ">
-                  <button>
-                    <Image
-                      src={gitHubImg}
-                      alt=""
-                      className="w-8 mr-4 shadow-lg ImgCards "
-                    />
-                  </button>
-                  <button>
-                    <Image src={vercelImg} alt="" className="w-7 ImgCards" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-full flex flex-wrap justify-evenly">
-          <div className="w-1/2 h-[200px] bg-slate-900 rounded-xl overflow-hidden  m-5 flex ">
-            <Image
-              src={projectImg}
-              alt=""
-              className="w-1/3 h-full opacity-80 "
-            />
-            <div className="flex flex-col justify-center items-center">
-              {" "}
-              <h2 className="m-2 text-lg font-semibold">E-commerce</h2>
-              <p className="pl-5 pr-5 ">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut
-                commodi blanditiis dolorum facere! Magnam ducimus eveniet
-                quisquam quibusdam provident velit? Quasi vitae obcaecati
-                dignissimos consequatur illo culpa numquam ducimus accusantium.
-              </p>
-              <div className="w-full flex justify-end mb-2">
-                <div className="w-1/4 ">
-                  <button>
-                    <Image
-                      src={gitHubImg}
-                      alt=""
-                      className="w-8 mr-4 shadow-lg ImgCards "
-                    />
-                  </button>
-                  <button>
-                    <Image src={vercelImg} alt="" className="w-7 ImgCards" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-full flex flex-wrap justify-evenly">
-          <div className="w-1/2 h-[200px] bg-slate-900 rounded-xl overflow-hidden  m-5 flex ">
-            <Image
-              src={projectImg}
-              alt=""
-              className="w-1/3 h-full opacity-80 "
-            />
-            <div className="flex flex-col justify-center items-center">
-              {" "}
-              <h2 className="m-2 text-lg font-semibold">E-commerce</h2>
-              <p className="pl-5 pr-5 ">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut
-                commodi blanditiis dolorum facere! Magnam ducimus eveniet
-                quisquam quibusdam provident velit? Quasi vitae obcaecati
-                dignissimos consequatur illo culpa numquam ducimus accusantium.
-              </p>
-              <div className="w-full flex justify-end mb-2">
-                <div className="w-1/4 ">
-                  <button>
-                    <Image
-                      src={gitHubImg}
-                      alt=""
-                      className="w-8 mr-4 shadow-lg ImgCards "
-                    />
-                  </button>
-                  <button>
-                    <Image src={vercelImg} alt="" className="w-7 ImgCards" />
-                  </button>
-                </div>
+              <div className="w-full md:w-1/4 m-2 flex justify-center items-center">
+                <button>
+                  <Image
+                    src={gitHubImg}
+                    alt=""
+                    className=" w-8 mr-4 shadow-lg ImgCards "
+                  />
+                </button>
+                <button>
+                  <Image src={vercelImg} alt="" className="w-7 ImgCards mb-1" />
+                </button>
               </div>
             </div>
           </div>
