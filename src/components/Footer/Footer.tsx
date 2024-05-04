046dcdda795b7b55f971db50cd8c34d1de742c1c
leandro mortarini <leandromortarini2@gmail.com>
2024-05-04 02:18:30 -0300

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,45 +9,38 @@ export const Footer = () => {
             href="https://flowbite.com/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+            <span className="self-center text-md font-semibold whitespace-nowrap text-gray-500">
+              leandromortarini2@gmail.com
             </span>
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                About
+              <a
+                href="https://www.linkedin.com/in/leandro-mortarini/"
+                className="hover:underline me-4 md:me-6"
+              >
+                Linkedin
               </a>
             </li>
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Privacy Policy
+              <a
+                href="https://github.com/leandromortarini2"
+                className="hover:underline me-4 md:me-6"
+              >
+                GitHub
               </a>
             </li>
+
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
+              <a href="https://wa.me/541126034427" className="hover:underline">
+                Wathsapp
               </a>
             </li>
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
-          </a>
-          . All Rights Reserved.
+          Portfolio Full Stack Developer
         </span>
       </div>
     </footer>
