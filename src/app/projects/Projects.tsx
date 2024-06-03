b9cd4ea71cd48f235c27f1a6aff058ebef3c7d48
leandro mortarini <leandromortarini2@gmail.com>
2024-06-03 18:12:39 -0300

    proyecto rindoor

--- src/app/projects/Projects.tsx
+++ src/app/projects/Projects.tsx
@@ -5,6 +5,7 @@ import codeImg from "@/assets/code.svg";
 import projectImg from "@/assets/Project1.png";
 import gitHubImg from "@/assets/gitHubWhite.png";
 import vercelImg from "@/assets/vercel.png";
+import rinDoor from "@/assets/Captura de pantalla 2024-05-31 192422.jpg";
 import AppMovies from "@/assets/AppMovies.jpg";
 import ShiftManager from "@/assets/gestorTurnos.jpg";
 import Ecommerce from "@/assets/Project1.png";
@@ -28,6 +29,48 @@ export const Projects = () => {
         {/* CARDS */}
         <div className="w-full flex  flex-wrap justify-evenly ">
           {" "}
+          <div className="w-3/4 flex flex-col md:flex-row md:w-3/4 lg:w-3/4 xl:w-1/2 md:h-[200px] bg-slate-900 rounded-xl overflow-hidden  m-5 shadow-lg shadow-teal-950">
+            <Image
+              src={rinDoor}
+              alt=""
+              className="w-full md:w-1/3 h-full opacity-80 "
+            />
+            <div className="flex flex-col justify-center items-center">
+              {" "}
+              <h2 className="  mt-2  lg:text-lg xl:font-semibold">Rin|Door</h2>
+              <p className="md:text-sm lg:text-md p-2 xl:pl-5 xl:pr-5 ">
+                <span className="text-teal-500">
+                  Next Js | Tailwind | Javascript | OAuth 2.0
+                </span>{" "}
+                <br />
+                Rindoor es una aplicacion donde los profecionales se suscriben y
+                pueden acceder a trabajos publicados por clientes, estos
+                clientes publican sus necesidades de forma gratuita y pueden
+                escoger el profesional que deseen dentro de los que se
+                postularon para el trabajo.
+              </p>
+              <div className="w-full md:w-1/4 m-2 flex justify-center items-center">
+                <Link href="https://github.com/leandromortarini2/Rindoor-2.0">
+                  <button>
+                    <Image
+                      src={gitHubImg}
+                      alt=""
+                      className=" w-8 mr-4 shadow-lg ImgCards "
+                    />
+                  </button>
+                </Link>
+                <Link href="https://rindoor-2-0.vercel.app/">
+                  <button>
+                    <Image
+                      src={vercelImg}
+                      alt=""
+                      className=" w-8 mr-4 shadow-lg ImgCards "
+                    />
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
           <div className="w-3/4 flex flex-col md:flex-row md:w-3/4 lg:w-3/4 xl:w-1/2 md:h-[200px] bg-slate-900 rounded-xl overflow-hidden  m-5 shadow-lg shadow-teal-950">
             <Image
               src={Ecommerce}
